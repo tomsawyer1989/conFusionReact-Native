ee87f52243b4fa136bd97cebbc3e397b76b907f4
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-01-05 22:16:09 -0500

    Components Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import { View } from 'react-native';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -7,12 +9,20 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      selectedDish: null,
     };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({selectedDish: dishId});
   }
 
   render() {
     return (
-        <Menu dishes={this.state.dishes} />
+        <View style={{flex:1}}>
+            <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+            <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        </View>
     );
   }
 }
