6c4b36377f5b28f82bada166892974b24d820f46
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-01-20 22:15:52 -0500

    Delete Favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FlatList, View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
@@ -10,7 +12,11 @@ const mapStateToProps = state => {
       dishes: state.dishes,
       favorites: state.favorites
     }
-  }
+}
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
 
 class Favorites extends Component {
 
@@ -23,16 +29,25 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
         
         const renderMenuItem = ({item, index}) => {
-    
+            const rightButton = [
+                {
+                    text: 'Delete', 
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: {uri: baseUrl + item.image}}}
                     />
+                </Swipeout>
             );
         };
 
@@ -54,10 +69,10 @@ class Favorites extends Component {
                     data={this.props.dishes.dishes.filter(dish => this.props.favorites.some(el => el === dish.id))}
                     renderItem={renderMenuItem}
                     keyExtractor={item => item.id.toString()}
-                    />
+                />
             );
         }
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
